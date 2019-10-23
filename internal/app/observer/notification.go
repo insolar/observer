d6ea3d484054fe65b8473b1d9252a7bb0c389184
hirama <crrrr@mail.ru>
2019-10-23 15:02:29 +0300

    SAIV-153: refactor

--- internal/app/observer/notification.go
+++ internal/app/observer/notification.go
@@ -1,3 +1,19 @@
+//
+// Copyright 2019 Insolar Technologies GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package observer
 
 import (
@@ -19,13 +35,3 @@ type NotificationStorage interface {
 type NotificationCollector interface {
 	Collect(*Record) *Notification
 }
-
-// NotificationType type of swap steps
-type NotificationType int
-
-//go:generate stringer -type=NotificationType
-const (
-	NotificationInvite NotificationType = iota + 1
-	NotificationContribution
-	NotificationDeactivate
-)
