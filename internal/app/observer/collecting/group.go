23f2bd4a9d5a465f3301465941203a25cf52b670
hirama <crrrr@mail.ru>
2019-11-25 19:19:42 +0300

    NOISSUE: refactoring

--- internal/app/observer/collecting/group.go
+++ internal/app/observer/collecting/group.go
@@ -9,97 +9,39 @@ import (
 	"github.com/insolar/observer/internal/app/observer"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	"reflect"
 )
 
 type GroupCollector struct {
-	log       *logrus.Logger
-	results   observer.ResultCollector
-	activates observer.ActivateCollector
-	halfChain observer.ChainCollector
-	chains    observer.ChainCollector
+	log *logrus.Logger
 }
 
 func NewGroupCollector(log *logrus.Logger) *GroupCollector {
-	results := NewResultCollector(isGroupCreationCall, successResult)
-	activates := NewActivateCollector(isGroupNew, isGroupActivate)
-	resultRelation := &RelationDesc{
-		Is:     isCoupledResult,
-		Origin: coupledResultOrigin,
-		Proper: isCoupledResult,
-	}
-	activateRelation := &RelationDesc{
-		Is:     isCoupledActivate,
-		Origin: coupledActivateOrigin,
-		Proper: isCoupledActivate,
-	}
-	userCreateGroupCall := &RelationDesc{
-		Is: isUserGroupCreateCall,
-		Origin: func(chain interface{}) insolar.ID {
-			request := observer.CastToRequest(chain)
-			return request.ID
-		},
-		Proper: isUserGroupCreateCall,
-	}
-	userRelation := &RelationDesc{
-		Is: func(chain interface{}) bool {
-			c, ok := chain.(*observer.Chain)
-			if !ok {
-				return false
-			}
-			return isUserGroupCreateCall(c.Parent)
-		},
-		Origin: func(chain interface{}) insolar.ID {
-			c, ok := chain.(*observer.Chain)
-			if !ok {
-				return insolar.ID{}
-			}
-			request := observer.CastToRequest(c.Parent)
-			return request.Reason()
-		},
-		Proper: func(chain interface{}) bool {
-			c, ok := chain.(*observer.Chain)
-			if !ok {
-				return false
-			}
-			return isUserGroupCreateCall(c.Parent)
-		},
-	}
 	return &GroupCollector{
-		results:   results,
-		activates: activates,
-		halfChain: NewChainCollector(userCreateGroupCall, activateRelation),
-		chains:    NewChainCollector(resultRelation, userRelation),
+		log: log,
 	}
 }
 
 func (c *GroupCollector) Collect(rec *observer.Record) *observer.Group {
-	res := c.results.Collect(rec)
-	act := c.activates.Collect(rec)
-	half := c.halfChain.Collect(rec)
-
-	if act != nil {
-		half = c.halfChain.Collect(act)
+	if rec == nil {
+		return nil
 	}
+	actCandidate := observer.CastToActivate(rec)
 
-	var chain *observer.Chain
-	if res != nil {
-		chain = c.chains.Collect(res)
-	}
-
-	if half != nil {
-		chain = c.chains.Collect(half)
-	}
-
-	if chain == nil {
+	if !actCandidate.IsActivate() {
 		return nil
 	}
 
-	coupleAct, coupleRes, request := c.unwrapGroupChain(chain)
+	act := actCandidate.Virtual.GetActivate()
 
-	g, err := c.build(coupleAct, coupleRes, request)
+	// TODO: import from platform
+	prototypeRef, _ := insolar.NewReferenceFromString("0111A7bz1ZzDD9CJwckb5ufdarH7KtCwSSg2uVME3LN9")
+	if !act.Image.Equal(*prototypeRef) {
+		return nil
+	}
+
+	g, err := c.build(actCandidate)
 	if err != nil {
-		log.Error(errors.Wrapf(err, "failed to build group"))
+		log.Error(errors.Wrapf(err, "failed to build transaction"))
 		return nil
 	}
 	return g
@@ -127,13 +69,9 @@ type Membership struct {
 	IsAnonymous  bool
 }
 
-func (c *GroupCollector) build(act *observer.Activate, res *observer.Result, req *observer.Request) (*observer.Group, error) {
-	if res == nil || act == nil {
+func (c *GroupCollector) build(act *observer.Activate) (*observer.Group, error) {
+	if act == nil {
 		return nil, errors.New("trying to create group from non complete builder")
-	}
-
-	if res.Virtual.GetResult().Payload == nil {
-		return nil, errors.New("group creation result payload is nil")
 	}
 
 	activate := act.Virtual.GetActivate()
@@ -170,68 +108,6 @@ func (c *GroupCollector) build(act *observer.Activate, res *observer.Result, req
 	return &resultGroup, nil
 }
 
-func isUserGroupCreateCall(chain interface{}) bool {
-	request := observer.CastToRequest(chain)
-	if !request.IsIncoming() {
-		return false
-	}
-
-	in := request.Virtual.GetIncomingRequest()
-	if in.Method != "CreateGroup" {
-		return false
-	}
-
-	if in.Prototype == nil {
-		return false
-	}
-	prototypeRef, _ := insolar.NewReferenceFromString("0111A5tDgkPiUrCANU8NTa73b7w6pWGRAUxJTYFXwTnR")
-	return in.Prototype.Equal(*prototypeRef)
-}
-
-func isGroupActivate(chain interface{}) bool {
-	activate := observer.CastToActivate(chain)
-	if !activate.IsActivate() {
-		return false
-	}
-	act := activate.Virtual.GetActivate()
-
-	// TODO: import from platform
-	prototypeRef, _ := insolar.NewReferenceFromString("0111A7bz1ZzDD9CJwckb5ufdarH7KtCwSSg2uVME3LN9")
-	return act.Image.Equal(*prototypeRef)
-}
-func isGroupCreationCall(chain interface{}) bool {
-	request := observer.CastToRequest(chain)
-	if !request.IsIncoming() {
-		return false
-	}
-
-	if !request.IsMemberCall() {
-		return false
-	}
-
-	args := request.ParseMemberCallArguments()
-	return args.Params.CallSite == "group.create" || args.Params.CallSite == "group.initialize"
-}
-func isGroupNew(chain interface{}) bool {
-	request := observer.CastToRequest(chain)
-	if !request.IsIncoming() {
-		return false
-	}
-
-	in := request.Virtual.GetIncomingRequest()
-	if in.Method != "New" {
-		return false
-	}
-
-	if in.Prototype == nil {
-		return false
-	}
-
-	// TODO: import from platform
-	prototypeRef, _ := insolar.NewReferenceFromString("0111A7bz1ZzDD9CJwckb5ufdarH7KtCwSSg2uVME3LN9")
-	return in.Prototype.Equal(*prototypeRef)
-}
-
 func (c *GroupCollector) initialGroupState(act *record.Activate) *Group {
 	g := Group{}
 	err := insolar.Deserialize(act.Memory, &g)
@@ -249,35 +125,6 @@ func (c *GroupCollector) initialGroupState(act *record.Activate) *Group {
 
 	}
 	return &g
-}
-
-func (c *GroupCollector) unwrapGroupChain(chain *observer.Chain) (*observer.Activate, *observer.Result, *observer.Request) {
-
-	half := chain.Child.(*observer.Chain)
-	coupledAct, ok := half.Child.(*observer.CoupledActivate)
-	if !ok {
-		log.Error(errors.Errorf("trying to use %s as *observer.Chain", reflect.TypeOf(chain.Child)))
-		return nil, nil, nil
-	}
-	if coupledAct.Activate == nil {
-		log.Error(errors.New("invalid coupled activate chain, child is nil"))
-		return nil, nil, nil
-	}
-	actRecord := coupledAct.Activate
-
-	coupledRes, ok := chain.Parent.(*observer.CoupledResult)
-	if !ok {
-		log.Error(errors.Errorf("trying to use %s as *observer.Chain", reflect.TypeOf(chain.Parent)))
-		return nil, nil, nil
-	}
-	if coupledRes.Result == nil {
-		log.Error(errors.New("invalid coupled result chain, child is nil"))
-		return nil, nil, nil
-	}
-	resRecord := coupledRes.Result
-	reqRecord := coupledRes.Request
-
-	return actRecord, resRecord, reqRecord
 }
 
 func groupUpdate(act *observer.Record) (*Group, error) {
