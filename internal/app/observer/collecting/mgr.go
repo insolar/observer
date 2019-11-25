72344f983f4bc8c1dbe327a770a86260b7ceb833
hirama <crrrr@mail.ru>
2019-11-25 19:34:38 +0300

    NOISSUE: refactor

--- internal/app/observer/collecting/mgr.go
+++ internal/app/observer/collecting/mgr.go
@@ -7,67 +7,15 @@ import (
 	"github.com/insolar/observer/internal/app/observer"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	"reflect"
 )
 
 type MGRCollector struct {
-	log       *logrus.Logger
-	results   observer.ResultCollector
-	activates observer.ActivateCollector
-	halfChain observer.ChainCollector
-	chains    observer.ChainCollector
+	log *logrus.Logger
 }
 
 func NewMGRCollector(log *logrus.Logger) *MGRCollector {
-	results := NewResultCollector(isMGRCreationCall, successResult)
-	activates := NewActivateCollector(isMGRNew, isMGRActivate)
-	resultRelation := &RelationDesc{
-		Is:     isCoupledResult,
-		Origin: coupledResultOrigin,
-		Proper: isCoupledResult,
-	}
-	activateRelation := &RelationDesc{
-		Is:     isCoupledActivate,
-		Origin: coupledActivateOrigin,
-		Proper: isCoupledActivate,
-	}
-	userCreateGroupCall := &RelationDesc{
-		Is: isGroupMGRCreateCall,
-		Origin: func(chain interface{}) insolar.ID {
-			request := observer.CastToRequest(chain)
-			return request.ID
-		},
-		Proper: isGroupMGRCreateCall,
-	}
-	userRelation := &RelationDesc{
-		Is: func(chain interface{}) bool {
-			c, ok := chain.(*observer.Chain)
-			if !ok {
-				return false
-			}
-			return isGroupMGRCreateCall(c.Parent)
-		},
-		Origin: func(chain interface{}) insolar.ID {
-			c, ok := chain.(*observer.Chain)
-			if !ok {
-				return insolar.ID{}
-			}
-			request := observer.CastToRequest(c.Parent)
-			return request.Reason()
-		},
-		Proper: func(chain interface{}) bool {
-			c, ok := chain.(*observer.Chain)
-			if !ok {
-				return false
-			}
-			return isGroupMGRCreateCall(c.Parent)
-		},
-	}
 	return &MGRCollector{
-		results:   results,
-		activates: activates,
-		halfChain: NewChainCollector(userCreateGroupCall, activateRelation),
-		chains:    NewChainCollector(resultRelation, userRelation),
+		log: log,
 	}
 }
 
@@ -119,12 +67,12 @@ func (c *MGRCollector) Collect(rec *observer.Record) *observer.MGR {
 		return nil
 	}
 
-	tx, err := c.build(actCandidate)
+	mgr, err := c.build(actCandidate)
 	if err != nil {
-		log.Error(errors.Wrapf(err, "failed to build transaction"))
+		log.Error(errors.Wrapf(err, "failed to build mgr"))
 		return nil
 	}
-	return tx
+	return mgr
 }
 
 func (c *MGRCollector) build(act *observer.Activate) (*observer.MGR, error) {
@@ -162,98 +110,6 @@ func (c *MGRCollector) build(act *observer.Activate) (*observer.MGR, error) {
 	}
 
 	return &resultProduct, nil
-}
-
-func isMGRCreationCall(chain interface{}) bool {
-	request := observer.CastToRequest(chain)
-	if !request.IsIncoming() {
-		return false
-	}
-
-	if !request.IsMemberCall() {
-		return false
-	}
-	args := request.ParseMemberCallArguments()
-	return args.Params.CallSite == "group.setMGR"
-}
-
-func isMGRActivate(chain interface{}) bool {
-	activate := observer.CastToActivate(chain)
-	if !activate.IsActivate() {
-		return false
-	}
-	act := activate.Virtual.GetActivate()
-
-	// TODO: import from platform
-	prototypeRef, _ := insolar.NewReferenceFromString("0111A6L4ytii4Z9jWLJpFqjDkH8ZRZ8HNscmmzsBF85i")
-	return act.Image.Equal(*prototypeRef)
-}
-
-func isMGRNew(chain interface{}) bool {
-	request := observer.CastToRequest(chain)
-	if !request.IsIncoming() {
-		return false
-	}
-
-	in := request.Virtual.GetIncomingRequest()
-	if in.Method != "New" {
-		return false
-	}
-
-	if in.Prototype == nil {
-		return false
-	}
-
-	// TODO: import from platform
-	prototypeRef, _ := insolar.NewReferenceFromString("0111A6L4ytii4Z9jWLJpFqjDkH8ZRZ8HNscmmzsBF85i")
-	return in.Prototype.Equal(*prototypeRef)
-}
-
-func isGroupMGRCreateCall(chain interface{}) bool {
-	request := observer.CastToRequest(chain)
-	if !request.IsIncoming() {
-		return false
-	}
-
-	in := request.Virtual.GetIncomingRequest()
-	if in.Method != "CreateMGRProduct" {
-		return false
-	}
-
-	if in.Prototype == nil {
-		return false
-	}
-	prototypeRef, _ := insolar.NewReferenceFromString("0111A6L4ytii4Z9jWLJpFqjDkH8ZRZ8HNscmmzsBF85i")
-	return in.Prototype.Equal(*prototypeRef)
-}
-
-func (c *MGRCollector) unwrapMGRChain(chain *observer.Chain) (*observer.Activate, *observer.Result, *observer.Request) {
-
-	half := chain.Child.(*observer.Chain)
-	coupledAct, ok := half.Child.(*observer.CoupledActivate)
-	if !ok {
-		log.Error(errors.Errorf("trying to use %s as *observer.Chain", reflect.TypeOf(chain.Child)))
-		return nil, nil, nil
-	}
-	if coupledAct.Activate == nil {
-		log.Error(errors.New("invalid coupled activate chain, child is nil"))
-		return nil, nil, nil
-	}
-	actRecord := coupledAct.Activate
-
-	coupledRes, ok := chain.Parent.(*observer.CoupledResult)
-	if !ok {
-		log.Error(errors.Errorf("trying to use %s as *observer.Chain", reflect.TypeOf(chain.Parent)))
-		return nil, nil, nil
-	}
-	if coupledRes.Result == nil {
-		log.Error(errors.New("invalid coupled result chain, child is nil"))
-		return nil, nil, nil
-	}
-	resRecord := coupledRes.Result
-	reqRecord := coupledRes.Request
-
-	return actRecord, resRecord, reqRecord
 }
 
 func mgrUpdate(act *observer.Record) (*MerryGoRound, error) {
