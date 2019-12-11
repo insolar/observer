91524d591c6b2e728c332317f9cfb9603ef3762b
Ilya Ozherelyev <ilya.ozherelyev@insolar.io>
2019-12-11 18:02:10 +0300

    INS-3939: drop not null

--- scripts/migrations/3_deposit_indexes.up.sql
+++ scripts/migrations/3_deposit_indexes.up.sql
@@ -3,3 +3,6 @@ create index if not exists idx_deposits_per_member
 
 create index if not exists idx_deposits_per_state
     on deposits (deposit_state);
+
+
+alter table deposits alter column member_ref drop not null;
