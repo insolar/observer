9b26998b33ac2ba08c6771b0c740d5ac316ecbac
Ilya Markin <amferiuss@gmail.com>
2020-01-20 10:35:41 +0300

    INS-4096: don't print password in fatal logs

--- internal/dbconn/connection.go
+++ internal/dbconn/connection.go
@@ -26,7 +26,10 @@ import (
 func Connect(cfg configuration.DB) (*pg.DB, error) {
 	opt, err := pg.ParseURL(cfg.URL)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to parse cfg.DB.URL")
+		// pg.ParseURL uses standard url.Parse
+		// witch fills url-string with password into error.
+		// So we can't use errors.Wrap here and print error above in code.
+		return nil, errors.New("failed to parse cfg.DB.URL")
 	}
 	opt.PoolSize = cfg.PoolSize
 	return pg.Connect(opt), nil
