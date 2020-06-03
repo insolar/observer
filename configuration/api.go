081e7867dcbc612dba87603596f42d36a88094ec
Sergey Kudasov <sergey.kudasov@insolar.io>
2020-06-03 20:38:17 +0300

    MN-410: fix default cfgs for jepsen

--- configuration/api.go
+++ configuration/api.go
@@ -43,7 +43,7 @@ func (API) Default() *API {
 	return &API{
 		Listen: ":0",
 		DB: DB{
-			URL:             "postgres://postgres@localhost/postgres?sslmode=disable",
+			URL:             "postgres://observer:observer@localhost/observer?sslmode=disable",
 			Attempts:        5,
 			AttemptInterval: 3 * time.Second,
 		},
--- configuration/api_extended.go
+++ configuration/api_extended.go
@@ -26,7 +26,7 @@ func (APIExtended) Default() *APIExtended {
 		API: API{
 			Listen: ":0",
 			DB: DB{
-				URL:             "postgres://postgres@localhost/postgres?sslmode=disable",
+				URL:             "postgres://observer:observer@localhost/observer?sslmode=disable",
 				Attempts:        5,
 				AttemptInterval: 3 * time.Second,
 			},
