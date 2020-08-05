d9252f3624252e299a89e65cf9f78b6f3cac6b70
maximshalavin <shalavinms@gmail.com>
2020-08-05 16:58:55 +0300

    MN-631: validate contract version

--- internal/app/observer/grpc/version.go
+++ internal/app/observer/grpc/version.go
@@ -5,13 +5,13 @@ import (
 	"strconv"
 
 	"github.com/insolar/insolar/ledger/heavy/exporter"
+	"github.com/insolar/mainnet/application/appfoundation"
 	"google.golang.org/grpc/metadata"
 )
 
 func getCtxWithClientVersion(requestCtx context.Context) context.Context {
 	ctxMD := metadata.AppendToOutgoingContext(requestCtx, exporter.KeyClientType, exporter.ValidateContractVersion.String())
 	ctxMD = metadata.AppendToOutgoingContext(ctxMD, exporter.KeyClientVersionHeavy, strconv.Itoa(exporter.AllowedOnHeavyVersion))
-	// TODO set real version contract https://insolar.atlassian.net/browse/MN-631
-	return metadata.AppendToOutgoingContext(ctxMD, exporter.KeyClientVersionContract, "2")
+	return metadata.AppendToOutgoingContext(ctxMD, exporter.KeyClientVersionContract, strconv.Itoa(appfoundation.AllowedVersionSmartContract))
 
 }
